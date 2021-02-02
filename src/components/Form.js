e25eddf72c8ee8f64869aae31a49752fa3d60b5d
Jason Zhou <jzhou35@buffalo.edu>
2021-02-02 18:36:56 -0500

    commit tree 6743af83965e2084e7d2a73fef7fcaa17a8359cd

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,11 +3,17 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
-
+  function handleFirstNameChange(event) {
+    setFirstName(event.target.value);
+  }
+  
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
+  } 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <input type="text" value={lastName} onChange={handleLastNameChange}/>
       <button type="submit">Submit</button>
     </form>
   );
